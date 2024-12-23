c3891931fd12abd8439d1868832c1a04e30569f7
Ben Caunt <ben@warmhub.com>
2024-12-23 00:33:48 -0500

    add flow control

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,8 @@ async fn main() -> ! {
 
     let port = serialport::new("/dev/ttyUSB0", 115_200)
         .timeout(Duration::from_millis(10))
+        .flow_control(serialport::FlowControl::None)
+
         .open().expect("Failed to open port");
 
     let mut rplidar = RplidarDevice::with_stream(port);
